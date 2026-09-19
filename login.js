1ae6069ad7e87f4d40873b7a260f381e38281c4b
tansang-hub <specdown1341@gmail.com>
2026-09-19 10:33:10 +0700

    Delete login validation function
    Remove validateLogin function and module exports.

--- login.js
+++ login.js
@@ -1,7 +1,1 @@
-function validateLogin(username, password) {
-    return username === 'admin' && password === '123';
-}
 
-if (typeof module !== 'undefined' && module.exports) {
-    module.exports = { validateLogin };
-}
