b559895b18f0d069975b9a1d7469907ebe4c2c82
tansang-hub <specdown1341@gmail.com>
2026-09-19 10:33:21 +0700

    Delete validateLogin tests from login.test.js
    Remove tests for validateLogin function.

--- login.test.js
+++ login.test.js
@@ -1,11 +1,1 @@
-const { validateLogin } = require('./auth');
 
-describe('Kiểm tra hàm validateLogin', () => {
-    test('Đúng admin và 123 trả về true', () => {
-        expect(validateLogin('admin', '123')).toBe(true);
-    });
-
-    test('Sai mật khẩu trả về false', () => {
-        expect(validateLogin('admin', 'wrong')).toBe(false);
-    });
-});
