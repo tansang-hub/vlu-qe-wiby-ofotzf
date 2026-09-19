8e7ffe905cbccb2802b0adf77e9e7713479dbaa2
tansang-hub <specdown1341@gmail.com>
2026-09-19 10:35:59 +0700

    Update login.test.js

--- login.test.js
+++ login.test.js
@@ -1,1 +1,12 @@
+const { validateLogin } = require('./auth');
+
+describe('Kiểm tra hàm validateLogin', () => {
+    test('Nên trả về true khi nhập đúng admin và 123', () => {
+        expect(validateLogin('admin', '123')).toBe(true);
+    });
+
+    test('Nên trả về false khi sai mật khẩu', () => {
+        expect(validateLogin('admin', 'wrong')).toBe(false);
+    });
+});
 
